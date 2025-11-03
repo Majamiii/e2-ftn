d85898701b9b40776fa796d4f897adbc807c366e
Majamiii <maja.milovic05@gmail.com>
2025-11-03 22:11:40 +0100

    checkpoint. bug fixed on minimax logic - but still not working.

--- sit-predmeti/algoritmi-1/reversi/minimax.py
+++ sit-predmeti/algoritmi-1/reversi/minimax.py
@@ -1,6 +1,140 @@
 from state import State
-
+import time
+import copy
 
 class Game():
+    __slots__ = ['_current_state', '_player_turn']
+    def __init__(self):
+        self._current_state = None
+        self._player_turn = 'X'
+        self.initialize_game()
+    def initialize_game(self):
+        self._current_state = State()
+        self._player_turn = 'X'
+
+    # player O (AI) is max
+    def max(self, depth=0):
+        #   return 0,0,0
+
+        depth += 1
+
+        maxv = -1500
+        px = None
+        py = None
+        result, winner = self._current_state.is_end('O')
+
+        if result:
+            if winner == 'X':
+                return -1000, 0, 0
+            elif winner == 'O':
+                return 1000, 0, 0
+            else:
+                return 0, 0, 0
+            
+        if depth>3:
+            maxv = self._current_state.heuristics('O')
+            #   self._current_state.clean_available_moves('O')
+            return maxv, 0, 0
+            
+        # game is not over
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._current_state.get_value(i,j) == '*':
+                    og_board = copy.deepcopy(self._current_state._board)
+                    self._current_state.set_value(i,j,'O')
+                    self._current_state.reset_available_moves('O')
+                    (m, min_i, min_j) = self.min(depth)
+                    if m>maxv:
+                        maxv = m
+                        px = i
+                        py = j
+                    self._current_state._board = og_board
+        return maxv, px, py
+
+    # player X (human player) is min
+    def min(self, depth = 0):
+        #   return 0,0,0
+        depth += 1
+
+        minv = 1500
+        px = None
+        py = None
+        result, winner = self._current_state.is_end('X')
+
+        if result:
+            if winner == 'X':
+                return -1000, 0, 0
+            elif winner == 'O':
+                return 1000, 0, 0
+            else:
+                return 0, 0, 0
+            
+        if depth>3:
+            minv = self._current_state.heuristics('X')
+            #   self._current_state.clean_available_moves('O')
+            return minv, 0, 0
+            
+        # game is not over
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._current_state.get_value(i,j) == '*':
+                    og_board = copy.deepcopy(self._current_state._board)
+                    self._current_state.set_value(i,j,'X')
+                    self._current_state.reset_available_moves('X')
+                    (m, min_i, min_j) = self.max(depth)
+                    if m<minv:
+                        minv = m
+                        px = i
+                        py = j
+                    self._current_state._board = og_board
+        return minv, px, py
+    
+
     def play(self):
-        pass
+        while True:
+            self._current_state.reset_available_moves(self._player_turn)
+            print("Player ", self._player_turn, "turn")
+            result, winner = self._current_state.is_end(self._player_turn)
+            print(self._current_state)
+
+            if result:
+                if winner == 'X':
+                    print('The winner is X!')
+                elif winner == 'O':
+                    print('The winner is O!')
+                else:
+                    print("It's a tie!")
+
+                self.initialize_game()
+                return
+            
+            if self._player_turn == 'X':
+                while True:
+                    start = time.time()
+                    #(m, px, py) = self.min()
+                    end = time.time()
+                    print("Evaluation time: ", end-start)
+                    #   print("Recommended move: ", px, py)
+
+                    qy = int(input("Set x coordinate: "))
+                    if qy<0 or qy>7:
+                        print("Value not in range [0,7], try again!")
+                        continue
+                    qx = int(input("Set y coordinate: "))
+                    if qx<0 or qx>7:
+                        print("Value not in range [0,7], try again!")
+                        continue
+
+                    if self._current_state.is_move_valid(qx, qy, 'X'):
+                        self._current_state.set_value(qx,qy,'X')
+                        self._player_turn = 'O'
+                        print(self._current_state)
+                        break
+                    else:
+                        print('The move is not valid! Try again.')
+
+
+            else:
+                (m, px, py) = self.max()
+                self._current_state.set_value(px,py,'O')
+                self._player_turn = 'X'
--- sit-predmeti/algoritmi-1/reversi/state.py
+++ sit-predmeti/algoritmi-1/reversi/state.py
@@ -3,7 +3,6 @@ class State:
         self._board = []
         for i in range(0,8):
             self._board.append(8 * ['-'])
-        
         self._board[3][3] = 'X'     # X is black - starting the game - the player
         self._board[4][4] = 'X'
         self._board[3][4] = 'O'     # computer
@@ -11,16 +10,76 @@ class State:
 
     def get_value(self, i, j):
         return self._board[i][j]
+    
     def set_value(self,i,j,value):
         self._board[i][j] = value
+        self.update_board(i,j,value)
+
+    def taken_corners(self, player):
+        corners = 0
+        if self._board[0][0] == player:
+            corners +=1
+        if self._board[7][7] == player:
+            corners +=1
+        if self._board[0][7] == player:
+            corners +=1
+        if self._board[7][0] == player:
+            corners +=1
+        return corners
+    
+    def taken_corners_opponent(self, move):
+        corners = 0
+        if move == 'X': player = 'O'
+        elif move == 'O': player = 'X'
+        if self._board[0][0] == player:
+            corners +=1
+        if self._board[7][7] == player:
+            corners +=1
+        if self._board[0][7] == player:
+            corners +=1
+        if self._board[7][0] == player:
+            corners +=1
+        return corners
+    
+    def taken_pieces(self, player):
+        pieces = 0
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._board[i][j] == player:
+                    pieces += 1
+        return pieces
+    
+    def taken_pieces_opponent(self, move):
+        if move == 'X': player = 'O'
+        elif move == 'O': player = 'X'
+        pieces = 0
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._board[i][j] == player:
+                    pieces += 1
+        return pieces
+    
+    def taken_around_corners(self, player):
+        return 0
+
     def is_move_valid(self,i,j,move):
         move = move.upper()
         if move not in 'XO':
             return False
-        if self._board[i][j] != '-':
+        self.reset_available_moves(move)
+        if self._board[i][j] != '*':
             return False
         return True
-    def mark_available_moves(self, player):     # player = 'X' or 'O'
+    
+    def available_moves(self):
+        available = 0
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._board[i][j] == '*': available += 1
+        return available
+
+    
+    def mark_available_moves(self, player='X'):     # player = 'X' or 'O'
         #   print out '*' for available moves
         move = player
         for i in range(0,8):
@@ -118,8 +177,119 @@ class State:
                                 break
                     if count_found:
                         count_found = 0
+
+    def reset_available_moves(self, player='X'):
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._board[i][j] == '*':
+                    self._board[i][j] = '-'
+        self.mark_available_moves(player)
+        return self._board
+    
+    def clean_available_moves(self, player='X'):
+        for i in range(0,8):
+            for j in range(0,8):
+                if self._board[i][j] == '*':
+                    self._board[i][j] = '-'
+
+    def update_board(self, qx, qy, value, first=True, direction=1):
+        # directions are clockwise from 1 to 8 - we should always keep going in the same direction
+
+        if first:
+            for direction in range(1,9):
+                # dir=1, we are checking bottom-up
+                if direction==1 and qx>0:
+                    if self._board[qx-1][qy] not in [value, '-', '*'] and self._board[qx-1][qy] in "XO":
+                        self._board[qx-1][qy] = value
+                        self.update_board(qx-1, qy, value, False, 1)
+
+                if direction == 2 and qx>0 and qy<6:
+                    if self._board[qx-1][qy+1] not in [value, '-', '*'] and self._board[qx-1][qy+1] in "XO":
+                        self._board[qx-1][qy+1] = value
+                        self.update_board(qx-1, qy+1, value, False, 2)
+                
+                if direction == 3 and qy<6:
+                    if self._board[qx][qy+1] not in [value, '-', '*'] and self._board[qx][qy+1] in "XO":
+                        self._board[qx][qy+1] = value
+                        self.update_board(qx, qy+1, value, False, 3)
+
+                if direction == 4 and qx<6 and qy<6:
+                    if self._board[qx+1][qy+1] not in [value, '-', '*'] and self._board[qx+1][qy+1] in "XO":
+                        self._board[qx+1][qy+1] = value
+                        self.update_board(qx+1, qy+1, value, False, 4)
+                
+                if direction == 5 and qx<6:
+                    if self._board[qx+1][qy] not in [value, '-', '*'] and self._board[qx+1][qy] in "XO":
+                        self._board[qx+1][qy] = value
+                        self.update_board(qx+1, qy, value, False, 5)
+                
+                if direction == 6 and qx<6 and qy>0:
+                    if self._board[qx+1][qy-1] not in [value, '-', '*'] and self._board[qx+1][qy-1] in "XO":
+                        self._board[qx+1][qy-1] = value
+                        self.update_board(qx+1, qy-1, value, False, 6)
+
+                if direction == 7 and qy>0:
+                    if self._board[qx][qy-1] not in [value, '-', '*'] and self._board[qx][qy-1] in "XO":
+                        self._board[qx][qy-1] = value
+                        self.update_board(qx, qy-1, value, False, 7)
+                
+                if direction == 8 and qx>0 and qy>0:
+                    if self._board[qx-1][qy-1] not in [value, '-', '*'] and self._board[qx-1][qy-1] in "XO":
+                        self._board[qx-1][qy-1] = value
+                        self.update_board(qx-1, qy-1, value, False, 8)
+
+        
+
+        else:
+            # not the first time entering the loop - we must keep going in the same direction
+            # dir=1, we are checking bottom-up
+            
+            if direction==1 and qx>0:
+                if self._board[qx-1][qy] not in [value, '-', '*'] and self._board[qx-1][qy] in "XO":
+                    self._board[qx-1][qy] = value
+                    self.update_board(qx-1, qy, value, False, 1)
+
+            if direction == 2 and qx>0 and qy<6:
+                if self._board[qx-1][qy+1] not in [value, '-', '*'] and self._board[qx-1][qy+1] in "XO":
+                    self._board[qx-1][qy+1] = value
+                    self.update_board(qx-1, qy+1, value, False, 2)
+            
+            if direction == 3 and qy<6:
+                if self._board[qx][qy+1] not in [value, '-', '*'] and self._board[qx][qy+1] in "XO":
+                    self._board[qx][qy+1] = value
+                    self.update_board(qx, qy+1, value, False, 3)
+
+            if direction == 4 and qx<6 and qy<6:
+                if self._board[qx+1][qy+1] not in [value, '-', '*'] and self._board[qx+1][qy+1] in "XO":
+                    self._board[qx+1][qy+1] = value
+                    self.update_board(qx+1, qy+1, value, False, 4)
+            
+            if direction == 5 and qx<6:
+                if self._board[qx+1][qy] not in [value, '-', '*'] and self._board[qx+1][qy] in "XO":
+                    self._board[qx+1][qy] = value
+                    self.update_board(qx+1, qy, value, False, 5)
+            
+            if direction == 6 and qx<6 and qy>0:
+                if self._board[qx+1][qy-1] not in [value, '-', '*'] and self._board[qx+1][qy-1] in "XO":
+                    self._board[qx+1][qy-1] = value
+                    self.update_board(qx+1, qy-1, value, False, 6)
+
+            if direction == 7 and qy>0:
+                if self._board[qx][qy-1] not in [value, '-', '*'] and self._board[qx][qy-1] in "XO":
+                    self._board[qx][qy-1] = value
+                    self.update_board(qx, qy-1, value, False, 7)
+            
+            if direction == 8 and qx>0 and qy>0:
+                if self._board[qx-1][qy-1] not in [value, '-', '*'] and self._board[qx-1][qy-1] in "XO":
+                    self._board[qx-1][qy-1] = value
+                    self.update_board(qx-1, qy-1, value, False, 8)
+        
+        return
+
+
+        
     def __str__(self):
-        self.mark_available_moves('X')
+        #   self.reset_available_moves()        # should be commented out
         ret = "\n\n"
         ret += "   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |\n"
         ret += "   ---------------------------------\n"
@@ -136,14 +306,37 @@ class State:
             ret += "\n"
             ret += "   ---------------------------------\n"
         return ret
-    def is_end(self):
+    
+    def is_end(self, player):
         found_option = False
+        self.reset_available_moves(player)
         for i in range(0,8):
             for j in range(0,8):
                 if self._board[i][j] == '*':
-                    found_option = True
-        if found_option: return True
-        return False
+                    found_option = True, None
+        if found_option: return False, None
 
-b = State()
-print(b)
+        # so no options were found for this player, the other is a winner
+        if player == 'X': return True, 'O'
+        elif player == 'O': return True, 'X'
+        else:
+            raise Exception("Error: Bug: No moves found but no winner either")
+        
+
+
+        
+    #   -------------------------------------------------
+    #   TACICS - HEURISTICS - THE MOST IMPORTANT FUNCTION
+    #   -------------------------------------------------
+
+    def heuristics(self, player):       
+        # use it simply like this for the maximizer
+        # and for the minimizer take the negative value of the return value of heuristics
+        score = 0
+        score += 100 * (self.taken_pieces(player) / self.taken_pieces_opponent(player))
+        score += 50 * self.taken_corners(player)  -  20*self.taken_corners_opponent(player)
+        score -= 30 * self.taken_around_corners(player)     #  TODO: implement this
+        score += 20 * self.available_moves()
+
+        return score
+        
